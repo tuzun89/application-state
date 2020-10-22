a6f71f3011d599123e449e772e09736201fb1d36
tuzun89 <to.uzunoglu@gmail.com>
2020-10-23 00:42:54 +0100

    try-catch added

--- vanilla-client/vanillaJS/app.js
+++ vanilla-client/vanillaJS/app.js
@@ -1,19 +1,49 @@
 const form = document.querySelector('form');
 const input = document.querySelector('#searchTerm');
+const resultsSection = document.querySelector('#results');
 
 const API_URL = 'http://www.omdbapi.com/?apikey=87ec8126&type=movie&s='
 
 form.addEventListener('submit', formSubmission);
 
-function formSubmission(event) {
+async function formSubmission(event) {
     event.preventDefault();
     const searchTerm = input.value;
-    getResults(searchTerm);
+    try {
+        const results = await getResults(searchTerm);
+        getResults(results);
+    } catch (error) {
+        showError(error);
+    }
 }
 
-function getResults(searchTerm) {
+async function getResults(searchTerm) {
     const url = `${API_URL}${searchTerm}`;
-    fetch(url).then(response => response.json()).then(data => {
-        console.log(data.Search);
-    });
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.Error) {
+        throw new Error(data.Error);
+    }
+    return data.Search;
+}
+
+function showResults(results) {
+    resultsSection.innerHTML = results.reduce((html, element) => {
+        return html + `
+        <div class="card col-4" style="width: 18rem;">
+            <img class="card-img-top" src="${element.Poster}" alt="${element.Title}">
+            <div class="card-body">
+                <h5 class="card-title">${element.Title}</h5>
+                <p class="card-text">${element.Year}</p>
+            </div>
+        </div>
+        `;
+    }, '');
+}
+
+function showError(error) {
+    resultsSection.innerHTML = `
+    <div class="alert alert-danger col" role="alert">
+        ${error.message}
+    `;
 }
