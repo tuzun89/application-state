0d61147a2c928b2af82128776d819b0bc51400d6
tuzun89 <to.uzunoglu@gmail.com>
2020-11-25 13:45:30 +0000

    adding comments

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
-What is Application State?
+Film Finder
 ---
-* [ ] Build a Movie Search App with Vanilla JS
+* [ ] Build a movie search application with basic JS.
   * [x] Get movie search term from user
   * [x] fetch API with search term
   * [x] Add search results to page
   * [x] Click result to add to "watch later" list
-  * [ ] Indicate results that are already in "watch later" list (do later...)
-* [x] What _is_ state?
-* [x] Refactor app to use single state object
-* [x] Refactor app to use a render/view function
-* [ ] Re-write app with Vue.js
-# application-state
+  * [ ] Indicate results that are already in "watch later" list
--- vanillaJS/app.js
+++ vanillaJS/app.js
@@ -12,16 +12,17 @@ facility where only the differences between the 'states'
 are rendered.
 */
 
-// List of variables used with refs to HTML tags & ids.
+/* List of variables representing page elements 
+used with refs to HTML tags & ids.*/
 const form = document.querySelector('form');
-const input = document.querySelector('#searchTerm');
+const input = document.querySelector('#searchTerm'); //input box on html
 const resultsSection = document.querySelector('#results');
 const watchListSection = document.querySelector('#watch-list');
 
 // API template URL.
 const API_URL = 'http://www.omdbapi.com/?apikey=87ec8126&type=movie&s='
 
-// Checks for user input
+// Listens for user input and calls forSubmission
 form.addEventListener('submit', formSubmission);
 
 function formSubmission(event) {
@@ -30,26 +31,26 @@ function formSubmission(event) {
     const searchTerm = input.value;
     // If getResults() throws error and catch here
     try {
-        getResults(searchTerm)
+        getResults(searchTerm)  // user input passed into getResults
             .then(showResults);
     } catch (error) {
-        console.error(error);
+        showError(error);
     }
 }
 
 function getResults(searchTerm) {
-    const url = `${API_URL}${searchTerm}`;
+    const url = `${API_URL}${searchTerm}`;  // URL constructor, adds user input to end of API_URL var.
     return fetch(url, {
         //method: 'POST'
     })
-        .then(response => response.json())
-        .then(data => data.Search);
-    /*
+        .then(response => response.json()) // fetch returns response from API and turn into json.
+        .then(data => data.Search); // get search results from API
+
+    // implement working error handling, maybe use aysnc/await?
     if (data.Error) {
         throw new Error(data.Error);
     }
     return data.Search;
-    */
 }
 
 function showError(error) {
@@ -60,43 +61,35 @@ function showError(error) {
     `;
 }
 
-function showResults(results) {
+function showResults(results) { // array of results passed in to showResults
     console.log(results);
     resultsSection.innerHTML = '';
     let html = '';
-    results.forEach(movie => {
-        html += getMovieTemplate(movie, 4);
+    results.forEach(movie => { // iterate over array of results
+        html += getMovieTemplate(movie, 4); // calls html template to replace whole page with new html
     });
-    //console.log(html);
+    console.log(html);
     resultsSection.innerHTML = html;
     const watchListButtons = document.querySelectorAll('.watch-list-button');
     watchListButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            const { id } = button.dataset; //destructuring
+        button.addEventListener('click', (event) => { // 
+            const { id } = button.dataset; //destructuring html data attributes
             const movie = results.find(movie => movie.imdbID === id);
             watchListSection.innerHTML = watchListSection.innerHTML
-                + getMovieTemplate(movie, 12, false);
+                + getMovieTemplate(movie, 12, false); // add movie to watchlist using template
         });
     });
 }
 
-/*
-Still to implement 'remove' button
-on the 'Watch Later' section
-*/
-function removeWatchList() {
-    const index = this.watchList.indexOf(movie);
-    this.watchList.splice(index, 1);
-}
-
+// bootstrap card filled with movie value and properties from API call.
 function getMovieTemplate(movie, cols, button = true) {
     return `
-    <div class="card col${cols}" style="width: 18rem;">
+    <div class="card col${cols}" style="width: 16em;"> // cols
         <img class="card-img-top" src="${movie.Poster}" alt="${movie.Title}">
         <div class="card-body">
             <h5 class="card-title">${movie.Title}</h5>
             <p class="card-text">${movie.Year}</p>
-            ${button ?  //template literal with ternary operator inside
+            ${button ?  //template literal with ternary operator inside for button
             `<button data-id="${movie.imdbID}" type="button" class="btn btn-outline-secondary 
                 btn-lg btn-block
                 watch-list-button">Add to Watchlist</button>`
@@ -105,4 +98,11 @@ function getMovieTemplate(movie, cols, button = true) {
         </div>
     </div>
     `
+}
+
+function removeWatchList() {
+    /*
+Still to implement 'remove' button
+on the 'Watch Later' section
+*/
 }
